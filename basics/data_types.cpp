f65fbf936235df2d6a2e4ab8da325a7bc1b00a1f
harshitha <valluruharshitha2005@gmail.com>
2025-07-15 12:44:18 +0530

    Intial commit

---
+++ basics/data_types.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int num=9;
+    float dec=2.54;
+    char aplhabet='A';
+    string str="Harshitha valluru";
+ cout << num <<endl<<dec<<endl<< aplhabet<< endl<<str<<endl;
+return 0;
+}
---
+++ basics/name.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string name;
+    cout <<"Enter your name:"<<endl;
+  getline(cin,name);
+    cout<< "Hi "<< name << "!!"<<endl;
+return 0;
+}
